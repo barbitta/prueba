a5837d10e39fbbf3d90ee09cd052dfa5a5bc882e
Raul <barbitta@uyconsulting.com.uy>
2020-10-29 16:42:36 -0300

    ajuste commit

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 library(testthat)
 library(fars)
 
 test_check("fars")
 
-=======
-library(testthat)
-library(fars)
-
-test_check("fars")
-
->>>>>>> 4f9a9043af615a92f9cb7b080c3637ec4ac6f5b8
